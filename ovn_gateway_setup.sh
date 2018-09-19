146a390f09a2179dcbdb4a95ca710d38e7a7dd9c
Rajat Chopra <rchopra@redhat.com>
2018-09-19 16:14:28 -0400

    fix iface id

--- ovn_gateway_setup.sh
+++ ovn_gateway_setup.sh
@@ -10,7 +10,7 @@ ovs-vsctl add-br br-win
 ovs-vsctl add-port br-win vxlan-win1 -- set interface vxlan-win1 type=vxlan  options:remote_ip=${REMOTE_IP} options:key=flow
 
 ovs-vsctl add-port br-win patch-br-win-int -- set interface patch-br-win-int type=patch options:peer=patch-br-int-win
-ovs-vsctl add-port br-int patch-br-int-win -- set interface patch-br-int-win type=patch options:peer=patch-br-win-int external-ids:iface-id=${NODE_NAME}_win
+ovs-vsctl add-port br-int patch-br-int-win -- set interface patch-br-int-win type=patch options:peer=patch-br-win-int external-ids:iface-id=${NODE_NAME}-win
 
 ovn-nbctl ls-add ext_${NODE_NAME}-win
 ovn-nbctl --may-exist lsp-add ext_${NODE_NAME}-win ${NODE_NAME}-win -- lsp-set-addresses ${NODE_NAME}-win unknown
